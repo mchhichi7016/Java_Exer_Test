d8971f3eb0f3f77471df7d8752dd84a8a4721b36
MINAMINO <causewaybar711@gmail.com>
2023-08-30 22:16:42 +0900

    Add files via upload
    17

--- HeyMyWorld.java
+++ HeyMyWorld.java
@@ -1,18 +1,5 @@
-public class Main {
+public class HeyMyWorld {
     public static void main(String[] args) {
-
-        int array[][] = new int[][]{{1,2},{2,3,4}};
-        int total = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array[i].length; j++) {
-                total += array[i][j];
-
-                System.out.println(array[i][j]);
-            }
-        }
-        System.out.println();
-        System.out.println(total);
-
-
+        System.out.println("hello");
     }
 }
