78263432aee051b6a1c299d2ac980815cc3a7ae8
MINAMINO <causewaybar711@gmail.com>
2023-08-30 22:03:25 +0900

    Update HeyMyWorld.java

--- HeyMyWorld.java
+++ HeyMyWorld.java
@@ -1,5 +1,18 @@
-public class HeyMyWorld {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        int array[][] = new int[][]{{1,2},{2,3,4}};
+        int total = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array[i].length; j++) {
+                total += array[i][j];
+
+                System.out.println(array[i][j]);
+            }
+        }
+        System.out.println();
+        System.out.println(total);
+
+
     }
 }
