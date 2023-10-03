ce4a27b8fb655cfb730d4dcd8f15be42b62cd5de
MINAMINO <causewaybar711@gmail.com>
2023-10-03 21:23:11 +0900

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,19 @@
+package valiable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// 将数据类型作为双型变量名pi值代入3.14
+		// 请在控制台上显示(请通过初始化方法创建变量
+		
+		double PI = 3.14;
+		//PI = 3.14;
+		System.out.println(PI);
+		
+		float height = 152.4f;
+		System.out.println(height);
+		
+
+	}
+
+}
