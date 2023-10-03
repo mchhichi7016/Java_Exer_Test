fd3e223eb37abeaedefbb2bd75292f77da17fdc4
MINAMINO <causewaybar711@gmail.com>
2023-10-03 21:23:53 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+package valiable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//変数を作る（宣言する）
+		int age;
+		//データの代入
+		age = 20;
+		//箱の中を確認する
+		System.out.println(age);
+		//変数名 = 值
+		age = 15;
+		System.out.println(age);
+				
+		
+		//変数の初期化
+		String name = "WangQi";
+		System.out.println(name);
+		
+		//mojiretsu
+		
+		System.out.println(name.length());
+		
+		
+		//my name is name age years old.
+		
+		System.out.println("my name is " + name +" "+ age + " years");
+		System.out.println(22);
+		System.out.println("二重引用符(\")");
+		
+		
+
+	}
+
+}
