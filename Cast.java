5f2bc65d81cdd08f39e1d348fb6ca0e4f5b6ddd7
MINAMINO <causewaybar711@gmail.com>
2023-10-03 21:24:24 +0900

    Create Cast.java

---
+++ Cast.java
@@ -0,0 +1,13 @@
+package valiable;
+
+public class Cast {
+
+	public static void main(String[] args) {
+		double myDouble = 9.78;
+		int myInt = (int)myDouble;
+		
+		System.out.println(myInt);
+
+	}
+
+}
