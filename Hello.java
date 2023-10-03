196eda467d62f50709c91ebae46ca8c8889dcf89
MINAMINO <causewaybar711@gmail.com>
2023-10-03 21:24:49 +0900

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		//System.out.println("Hello World!");
+		/*
+		   System.out.println("Hello Japan!"); 
+		   System.out.println("Hello Japan!");
+		   System.out.println("Hello Japan!"); 
+		   System.out.println("Hello Japan!");
+		 */
+		System.out.println("Hello Japan!");
+
+	}
+
+}
