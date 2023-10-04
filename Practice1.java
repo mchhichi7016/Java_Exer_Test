6c1e8363e989239cac2c689f37b89fafab32c921
MINAMINO <causewaybar711@gmail.com>
2023-10-04 09:38:21 +0900

    Create Practice1.java

---
+++ Practice1.java
@@ -0,0 +1,24 @@
+package review;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		
+		String name = "Alice"; 
+		System.out.println(name);
+		
+		
+		int[] scores = {40, 80, 120};
+		System.out.println("My scores is " + scores[1]);
+		
+		
+		int[][] array2D = {{30, 50, 10},{100,300,50}};
+		System.out.println("My score is " + array2D[1][1]);
+		
+		
+		
+
+	}
+
+}
