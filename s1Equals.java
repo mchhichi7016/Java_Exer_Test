f668a94c6977f254b7847d7af65fafb700cbacfb
MINAMINO <causewaybar711@gmail.com>
2023-10-08 17:11:46 +0900

    Create s1Equals.java

---
+++ s1Equals.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String S1 = new String("abc");
+        String S2 = new String("abc");
+
+        boolean x = S1.equals(S2);
+        System.out.println(x);
+    }
+}
