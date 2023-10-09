f9192f93ef605512fdc0f10595c15bc96874acdb
MINAMINO <causewaybar711@gmail.com>
2023-10-09 09:14:18 +0900

    Create staticNot.java

---
+++ staticNot.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        AbstractSample s = new ConcreteSample();
+        s.sample();
+
+   }
+   abstract class AbstractSample{
+        public void sample(){
+            System.out.println("A");
+            test();
+            System.out.println("C");
+        }
+        protected abstract void test();
+   }
+   class ConcreteSample extends AbstractSample{
+        protected void test(){
+            System.out.println("B");
+        }
+   }
+
+}
