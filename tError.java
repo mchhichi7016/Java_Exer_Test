a9c3b851ac63bc8e7abc1b739b84d29a3e57a6a2
MINAMINO <causewaybar711@gmail.com>
2023-10-09 09:57:53 +0900

    Create tError.java

---
+++ tError.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            int[] array = {};
+            array[0] = 10;
+            System.out.println("finish");
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("error");
+        }
+   }
+
+}
