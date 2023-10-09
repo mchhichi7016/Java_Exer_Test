ef8029555cbb8091820cd885262127126b5866dc
MINAMINO <causewaybar711@gmail.com>
2023-10-09 10:04:51 +0900

    Create argsLength0.java

---
+++ argsLength0.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            if (args.length == 0){
+                System.out.println("A");
+            }
+        }catch (NullPointerException e){
+            System.out.println("B");
+        }finally {
+            System.out.println("C");
+        }
+   }
+
+}
