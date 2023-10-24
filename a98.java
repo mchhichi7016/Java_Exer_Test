2d8b2654cba44e08115ffe2341b122492710fcd2
MINAMINO <causewaybar711@gmail.com>
2023-10-24 17:08:39 +0900

    Create a98.java

---
+++ a98.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        short a = 97;
+        int b = 98;
+
+        System.out.println((char) a + " ");
+        System.out.println((char) b);
+
+        Sample s = new Sample();
+        s.setValue("java");
+        //System.out.println(s.toString());
+
+    }
+}
