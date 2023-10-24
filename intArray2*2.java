4c48c20fc6b9f854a3ea49fd6a717e1a4acf29f4
MINAMINO <causewaybar711@gmail.com>
2023-10-24 17:07:25 +0900

    Create intArray2*2.java

---
+++ intArray2*2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        int[][] array = new int[2][4];
+        array[0] = new int[]{1,2,3,4};
+        array[1] = new int[]{1,2};
+
+        for (int[] a:array) {
+            for (int b:a) {
+                System.out.println(b);
+            }
+            System.out.println();
+        }
+
+    }
+}
