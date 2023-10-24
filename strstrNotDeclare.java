060145f4303459f265d515befb8b2b03999c98dd
MINAMINO <causewaybar711@gmail.com>
2023-10-24 17:14:34 +0900

    Create strstrNotDeclare.java

---
+++ strstrNotDeclare.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String str;
+        test(str);
+    }
+    private static void test(String str){
+        System.out.println(str);
+
+    }
+}
