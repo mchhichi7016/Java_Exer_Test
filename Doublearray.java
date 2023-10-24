cb8a25ed587b71b10a6f16135008f6cd5387c835
MINAMINO <causewaybar711@gmail.com>
2023-10-24 17:11:02 +0900

    Create Doublearray.java

---
+++ Doublearray.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        int[] array;
+        array = new int[]{};
+
+        int[] array2 =new int[3];
+        array2 = {1, 2, 3};
+
+        Double[] array3 = new Double[3] {};
+
+        int[] array4 = {array4[0],array4[1]};
+        
+        Double[] array5 = {null,1.0};
+
+    }
+}
